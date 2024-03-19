a1b348f1f21225b05e02e30ff5319502ab429763
alan lizardi <gil.lizardi@gmail.com>
2024-03-19 13:24:17 -0700

    V2.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-setInterval(() => {
+function fecha() {
     let fecha = new Date();
     let dia = fecha.getDate();
     let mes = fecha.getMonth() + 1;
@@ -18,4 +18,40 @@ setInterval(() => {
       mes +
       "/" +
       anio;
-  }, 1000);
+      return hora +
+      ":" +
+      minutos +
+      ":" +
+      segundos +
+      "-" +
+      dia +
+      "/" +
+      mes +
+      "/" +
+      anio;
+  }
+// setInterval(fecha, 1000);
+setInterval(function() {
+    document.getElementById("fecha").innerHTML = fecha();
+}, 1000);
+
+  const alumnos = [
+    [ "222217053", "Enriquez Grijalva Manuel","a222217053@unison.mx"],
+    [ "220212808", "Herrera Ceceña Angel David","a220212808@unison.mx"],
+    [ "221201308", "Lizardi Díaz Alan Gilberto","a221201308@unison.mx"]
+  ];
+
+  function buscarAlumno(expediente){
+    if (expediente.key === "Enter") {
+        expediente = document.getElementById("expediente").value;
+        for (let i = 0; i < alumnos.length; i++) {
+          if (alumnos[i][0] === expediente) {
+            const fechaActual = fecha();
+            document.getElementById("salida").innerHTML = (alumnos[i][1]);
+            document.getElementById("evento").innerHTML= ("Se ha inscrito al evento Hackathon 2025 - "+fechaActual);
+            document.getElementById("expediente").value = "";
+          }
+        }
+        return null;
+    }
+  }
